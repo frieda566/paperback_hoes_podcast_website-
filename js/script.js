13a9b1a5dedff498bac11b6b78cc07359026a6b7
Rachel Weller <Rachel.G.Weller@stud.leuphana.de>
2026-08-24 21:32:23 +0200

    added author page

--- js/script.js
+++ js/script.js
@@ -8,15 +8,18 @@ const buttons = document.querySelectorAll(".host-btn");
 
 buttons.forEach(button => {
     button.addEventListener("click", function () {
-        const card = this.closest(".host-card");
-        const info = card.querySelector(".host-info");
+        const info = this.nextElementSibling;
 
         info.classList.toggle("show");
 
-        this.textContent = info.classList.contains("show")
-            ? "Hide Favorites"
-            : "Show Favorites";
+        if (info.classList.contains("show")) {
+            this.textContent = "Hide " + this.dataset.name;
+        } else {
+            this.textContent = this.dataset.name;
+        }
+
     });
 });
+
 
 
