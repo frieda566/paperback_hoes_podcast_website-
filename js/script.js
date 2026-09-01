f060187dddaaac967443ec3266d24ef6ecbad736
frieda566 <frieda.schuetze@stud.leuphana.de>
2026-09-01 17:29:34 +0200

    try out menu interface on page

--- js/script.js
+++ js/script.js
@@ -4,6 +4,14 @@ themeBtn.addEventListener("click", function () {
     document.body.classList.toggle("dark-mode");
 });
 
+const menuToggle = document.getElementById("menuToggle");
+const mainNav = document.getElementById("mainNav");
+
+menuToggle.addEventListener("click", () => {
+    menuToggle.classList.toggle("active");
+    mainNav.classList.toggle("open");
+});
+
 const buttons = document.querySelectorAll(".host-btn");
 
 buttons.forEach(button => {
