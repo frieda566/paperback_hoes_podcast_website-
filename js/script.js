ee2519f4084f6603a095b8d42653d7ddb7e9aa7d
Rachel Weller <Rachel.G.Weller@stud.leuphana.de>
2026-09-12 18:49:31 +0200

    update dark mode

--- js/script.js
+++ js/script.js
@@ -7,9 +7,9 @@ if (themeBtn) {
         document.body.classList.toggle("dark-mode");
 
         if (document.body.classList.contains("dark-mode")) {
-            themeBtn.textContent = "☀️ Light Mode";
+            themeBtn.textContent = " Light Mode";
         } else {
-            themeBtn.textContent = "🌙 Dark Mode";
+            themeBtn.textContent = " Dark Mode";
         }
 
     });
